bab90b6c690d71040ad2b415f8aefe4ba8b1cf8d
khrisefzm <khrisefzm@gmail.com>
2024-05-31 16:39:01 -0500

    replace product.js

---
+++ data/options.js
@@ -0,0 +1,45 @@
+const options = {
+  navigation: [
+    {
+      title: "Ofertas de la semana",
+      linkTo: "./outlet.html",
+    },
+    {
+      title: "Cómo Comprar",
+      linkTo: "./how.html",
+    },
+    {
+      title: "Costos y tarifas",
+      linkTo: "./taxs.html",
+    },
+  ],
+  footer: [
+    {
+      title: "Ofertas de la semana",
+      linkTo: "./outlet.html",
+      opts: ["Laptops", "Audio", "Auticulares"],
+    },
+    {
+      title: "Cómo comprar",
+      linkTo: "./outlet.html",
+      opts: ["Formas de pago", "Envío", "Devoluciones"],
+    },
+    {
+      title: "Costos y tarifas",
+      linkTo: "./outlet.html",
+      opts: ["Impuestos", "Facturación"],
+    },
+    {
+      title: "Mis pedidos",
+      linkTo: "./outlet.html",
+      opts: ["Pedir Nuevamente", "Lista de deseos"],
+    },
+    {
+      title: "Garantia de entrega",
+      linkTo: "./outlet.html",
+      opts: [],
+    },
+  ],
+};
+
+export default options;
